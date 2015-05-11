c206ee1c8a2646b970ab25ad1787d04978b4277b
lorenzo.biava.91@gmail.com <lorenzo.biava.91@gmail.com>
2015-05-11 15:49:50 +0200

    [Semantics] Fixed StemmingTextCleaner for English

--- semantics/WSD/src/it/unito/nlplab/semantics/textcleaner/StemmingTextCleaner.java
+++ semantics/WSD/src/it/unito/nlplab/semantics/textcleaner/StemmingTextCleaner.java
@@ -2,14 +2,19 @@ package it.unito.nlplab.semantics.textcleaner;
 
 import it.unito.nlplap.semantics.utils.StopWordsTrimmer;
 
+import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 import org.apache.commons.lang.StringUtils;
+import org.tartarus.snowball.ext.EnglishStemmer;
 import org.tartarus.snowball.ext.ItalianStemmer;
 
 import rita.RiWordNet;
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.morph.WordnetStemmer;
 
 public class StemmingTextCleaner implements TextCleaner {
 
@@ -19,6 +24,8 @@ public class StemmingTextCleaner implements TextCleaner {
 	private StopWordsTrimmer swt;
 	private RiWordNet wn;
 	private ItalianStemmer itStemmer;
+	private EnglishStemmer enStemmer;
+	private String wordNetDir;
 
 	public StemmingTextCleaner(Locale language) {
 		this.language = language;
@@ -26,7 +33,8 @@ public class StemmingTextCleaner implements TextCleaner {
 		if (language == Locale.ITALIAN)
 			itStemmer = new ItalianStemmer();
 		else {
-			String wordNetDir = System.getenv("WNHOME");
+			enStemmer = new EnglishStemmer();
+			wordNetDir = System.getenv("WNHOME");
 			if (wordNetDir == null)
 				wordNetDir = WORDNET_DEFAULT_DIR;
 			this.wn = new RiWordNet(wordNetDir);
@@ -46,9 +54,24 @@ public class StemmingTextCleaner implements TextCleaner {
 				itStemmer.stem();
 				stems.add(itStemmer.getCurrent());
 			} else {
-				String pos = wn.getBestPos(term);
-				stems.add(getBestStem(term,
-						wn.getStems(term, (pos != null ? pos : RiWordNet.NOUN))));
+				enStemmer.setCurrent(term);
+				enStemmer.stem();
+				stems.add(enStemmer.getCurrent());
+
+				// RiWordNet Stemmer -> Not Working
+				// String pos = wn.getBestPos(term);
+				// stems.add(getBestStem(term,
+				// wn.getStems(term, (pos != null ? pos : RiWordNet.NOUN))));
+
+				// WordNet Stemmer -> Not Working
+				// Dictionary dict = new Dictionary(new URL("file://"+wordNetDir
+				// + "/dict"));
+				// dict.open();
+				// WordnetStemmer wnStemmer = new WordnetStemmer(dict);
+				//
+				// // String pos = wn.getBestPos(term);
+				// stems.add(getBestStem(term, (String[]) wnStemmer
+				// .findStems(term).toArray()));
 			}
 		}
 
