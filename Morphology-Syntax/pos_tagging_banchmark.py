b86dd8b8faa91920d20f75b1feba17f679d4b869
bl91 <bl91@hotmail.it>
2015-06-12 14:53:38 +0200

    [Morph-Syntax] [PoS] Improved Bad tags statistics

--- Morphology-Syntax/pos_tagging_banchmark.py
+++ Morphology-Syntax/pos_tagging_banchmark.py
@@ -29,6 +29,7 @@ def compare_sentence(tags, test_tags, bad_tags, tags_count=0, correct_tags_count
 
     return tags_count, correct_tags_count
 
+
 def tagger_benchmark(results, id, tagger_name, tagger, corpus):
     tags_count = 0
     bad_tags = []
@@ -54,24 +55,40 @@ def tagger_benchmark(results, id, tagger_name, tagger, corpus):
                 perc = (sentence_count / corpus_len) * 100
                 print('{:s}: {:d}/{:d} ({:.0f}%)'.format(tagger_name, sentence_count, corpus_len, perc))
                 progress_time = time.time()
-                #print(tagger_name+": "+sentence_count+"/"+corpus_len+" ("+strround((sentence_count/corpus_len)*100)+"%)")
-                #print(tags)
+                # print(tagger_name+": "+sentence_count+"/"+corpus_len+" ("+strround((sentence_count/corpus_len)*100)+"%)")
+                # print(tags)
 
     print('{:s}: ENDED ! Total time: {:.2f}s'.format(tagger_name, time.time() - start_time))
     results[id] = {'tags_count': tags_count, 'correct_tags_count': correct_tags_count, 'bad_tags': bad_tags}
+
+
+def get_bad_tags_stats(bad_tags):
+    """
+    Return a frequency count for bad tags, ordered descending
+    :param bad_tags: a list of tuples ((Word, CorrectTag), (Word, ComputedTag))
+    :return:
+    """
+    stats = dict()
+    for g, t in bad_tags:
+        key = (str(g[1]), str(t[1]))
+        if key in stats:
+            stats[key] += 1
+        else:
+            stats[key] = 1
+    return sorted(stats.items(), key=lambda e: e[1], reverse=True)
 
 if __name__ == '__main__':
 
     corpus_path = "data\\it\\it-universal-train.conll"
     test_corpus_path = "data\\it\\it-universal-test.conll"
 
-    corpus, corpus_digest = pos_tagging_utils.load_corpus(corpus_path)#, tag_field_index=4)
+    corpus, corpus_digest = pos_tagging_utils.load_corpus(corpus_path)  # , tag_field_index=4)
 
     # IMPORTANT NOTE: Accuracy may vary depending by PoS tags order !
-    #corpus_tags = pos_tagging_utils.get_corpus_tags(corpus)
+    # corpus_tags = pos_tagging_utils.get_corpus_tags(corpus)
     corpus_tags = pos_tagging_utils.universal_treebank_pos_tags
 
-    test_corpus, _ = pos_tagging_utils.load_corpus(test_corpus_path)#, tag_field_index=4)
+    test_corpus, _ = pos_tagging_utils.load_corpus(test_corpus_path)  # , tag_field_index=4)
 
     hmm_tagger = HMMTagger(corpus, corpus_tags, corpus_digest)
     hmm_tagger.opt_words_smoothing = 1
@@ -85,6 +102,8 @@ if __name__ == '__main__':
     mf_tags_count = 0
     mf_bad_tags = []
     mf_correct_tags_count = 0
+
+    enable_bad_tags_stats_output = 0
 
     manager = multiprocessing.Manager()
     results = manager.dict()
@@ -102,13 +121,18 @@ if __name__ == '__main__':
     proc_hmm.join()
     proc_mf.join()
 
-    #print(results.items())
+    # print(results.items())
     print("HMMTagger:")
     hmm_correct_tags_count = results[0]['correct_tags_count']
     hmm_tags_count = results[0]['tags_count']
     print('\tAccuracy: %.2f%%' % (hmm_correct_tags_count / hmm_tags_count * 100))
     print('\tGood tags: %d, Bad Tags: %d' % (hmm_correct_tags_count, len(results[0]['bad_tags'])))
     print('\tBad tags (correct, computed): %s' % results[0]['bad_tags'])
+    bad_tags_stats = get_bad_tags_stats(results[0]['bad_tags'])
+    if enable_bad_tags_stats_output:
+        [print('%s|%s|%d' % (k[0], k[1], v)) for k, v in bad_tags_stats]
+    else:
+        print('\tBad tags stats (correct, computed): %s' % bad_tags_stats)
 
     print("MostFreguentTagger:")
     mf_correct_tags_count = results[1]['correct_tags_count']
@@ -116,6 +140,10 @@ if __name__ == '__main__':
     print('\tAccuracy: %.2f%%' % (mf_correct_tags_count / mf_tags_count * 100))
     print('\tGood tags: %d, Bad Tags: %d' % (mf_correct_tags_count, len(results[1]['bad_tags'])))
     print('\tBad tags (correct, computed): %s' % results[1]['bad_tags'])
+    if enable_bad_tags_stats_output:
+        [print('%s|%s|%d' % (k[0], k[1], v)) for k, v in bad_tags_stats]
+    else:
+        print('\tBad tags stats (correct, computed): %s' % bad_tags_stats)
 
     print("Total tags: %d" % mf_tags_count)
 
